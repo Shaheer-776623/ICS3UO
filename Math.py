dfe6eba41e1447ee6b4c92bb197f90b12c3bb46a
Shaheer-776623 <776623@pdsb.net>
2024-09-19 10:06:09 -0400

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,17 @@
+print(35)
+amount = 7
+amount = amount + 2
+print("The amount is: ", amount)
+q = 9
+# using subtraction
+q = q - 1 
+# is the same thing
+q -= 1 
+# prints out "The value is now: 7"
+print("The value is now: ", q) 
+# using division
+q = q / 2 
+# same thing
+q /= 2 
+# prints out "The value is now: 1.75"
+print("The value is now: ", q) 
