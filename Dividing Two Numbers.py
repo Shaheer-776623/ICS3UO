0ba1dc2997a9ade93c66be01489552b3ed392a70
Shaheer-776623 <776623@pdsb.net>
2024-09-21 15:34:29 -0400

    Create Dividing Two Numbers.py

---
+++ Dividing Two Numbers.py
@@ -0,0 +1,6 @@
+num = input("Please input a whole number: ") 
+num = int(num) 
+num3 = input("Please input a whole number other than zero: ")
+num3 = int(num3)
+num2 = num / num3
+print("When", num, " is divided by 6", "the result is:", num2)
