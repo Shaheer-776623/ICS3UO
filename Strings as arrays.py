1b4052477a2ac23ec0aa30c021a6458a399efa36
Shaheer-776623 <776623@pdsb.net>
2024-10-07 10:30:42 -0400

    Create Strings as arrays.py

---
+++ Strings as arrays.py
@@ -0,0 +1,8 @@
+progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+space_count = 0
+
+for char in progname:
+    if char == ' ':
+      space_count += 1
+
+print("Number of spaces:", space_count)
