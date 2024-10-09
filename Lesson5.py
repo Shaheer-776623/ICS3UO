79bd161008d363325bb3429523884bb9fb32791e
Shaheer-776623 <776623@pdsb.net>
2024-10-09 10:50:10 -0400

    Update Lesson5.py

--- Lesson5.py
+++ Lesson5.py
@@ -2,9 +2,22 @@ import math
 
 n = int(input("Please input a value for n: "))
 
-print ("Counting form j = 1 to %d:\n" %n)
+print ("Counting from j = 1 to %d:\n" %n)
 
 print("j    tri    factorial")
 print("--------------------------")
-for i in range (1, n + 1):
-  print("%d    %d        %d" %(i, i**2, math.factorial(i)))
+
+total = 1
+count = 1
+
+total2 = 1
+count2 = 0
+
+  
+for n in range (1, n + 1):
+  while (count < n) and (count2 <= n):
+    count = count + 1
+    total = total + count
+    count2 = count2 + 1
+    total2 = total2 * count2
+    print("%d %5d %10d" %(n - 1 , total, total2))
