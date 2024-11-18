6e9eaf7e033bf5914d8d4639e02a68ceef0b2df8
Shaheer-776623 <776623@pdsb.net>
2024-11-18 10:06:13 -0500

    Create U5L1P2.py

---
+++ U5L1P2.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(60):
+    t.forward(100)
+    t.left(87)
+    
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(60):
+  t.forward(100)
+  t.left(123)
+
+    
+
+
+
